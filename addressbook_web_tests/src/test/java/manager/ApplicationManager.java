db4170a20af8751d425f7c4aeef4f5f55148cabf
Karpova <iokarpova@lanit.ru>
2025-10-07 21:40:53 +0300

    Добавление комментариев

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -27,6 +27,7 @@ public class ApplicationManager {
       }
       //Разлогин и закрытие браузера
       Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+      //Открытие страницы
       driver.get("http://localhost/addressbook/");
       driver.manage().window().setSize(new Dimension(1140, 1032));
       //Логин
