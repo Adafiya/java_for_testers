6bc47ea8b4433454bf55622088f6496362c54b43
Karpova <iokarpova@lanit.ru>
2025-11-08 21:40:15 +0300

    8. Интерфейс командной строки и сетевые интерфейсы. Все лекции модуля

---
+++ mantis_tests/src/test/java/ru/stqa/mantis/common/CommonFunctions.java
@@ -0,0 +1,28 @@
+package common;
+
+import java.util.Random;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CommonFunctions {
+
+  //Сгенерировать рандомный String в n символов
+  public static String randomString(int n) {
+    var rnd = new Random();
+    Supplier<Integer> randomNumbers = () -> rnd.nextInt(26);
+    var result = Stream.generate(randomNumbers)
+        .limit(n)
+        .map(i -> 'a' + i) //передаем i и получаем a+i
+        .map(Character::toString) //преобразуем символ в строку
+        .collect(Collectors.joining()); //собираем строчки вместе
+    /*
+    var rnd = new Random();c
+    var result = "";
+    for (int i = 0; i < n; i++) {
+      result = result + (char) ('a' + rnd.nextInt(26));
+    }
+     */
+    return result;
+  }
+}
---
+++ mantis_tests/src/test/java/ru/stqa/mantis/test/MailTests.java
@@ -0,0 +1,4 @@
+package ru.stqa.mantis.test;
+
+public class MailTests {
+}
